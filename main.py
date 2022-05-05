245f4b7e14f9ce689a3f339924446816bf9aeb49
jurij1 <96693230+Dogegowow@users.noreply.github.com>
2022-05-05 14:30:40 +0000

    boljsi setup

--- main.py
+++ main.py
@@ -10,25 +10,31 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   print(dir(motor))
-  #test.a()
   return render_template("index.html")
 
 @app.route("/motor")
-def motor():
+def run_motor():
   print("Motor pognan")
-  DIR = 20   # Direction GPIO Pin
-  STEP = 21  # Step GPIO Pin
+
+  #motor1
+  DIR = 16   # Direction GPIO Pin
+  STEP = 20  # Step GPIO Pin
   MOS = 23
   #motor2
   DIR2 = 5
   STEP2 = 6
-  MOS2 = 24
+  MOS2 = 25
+  #motor3
+  DIR3 = 26 
+  STEP3 = 19
+  MOS3 = 24
   #settings
   CW = 1     # Clockwise Rotation
   CCW = 0    # Counterclockwise Rotation
   SPR = 200   # Steps per Revolution (360 / 7.5)
-  motor.setup()
+
+  motor.setup(STEP, DIR, MOS)
   motor.run(STEP, DIR, CCW, MOS, 2)
   return redirect(url_for("home"))
-
+  
 app.run(host='0.0.0.0', port=8080)
--- motor.py
+++ motor.py
@@ -1,31 +1,12 @@
 from time import sleep
 import RPi.GPIO as GPIO
-def setup():
-    #motor1
-    DIR = 20   # Direction GPIO Pin
-    STEP = 21  # Step GPIO Pin
-    MOS = 23
-    #motor2
-    DIR2 = 5
-    STEP2 = 6
-    MOS2 = 24
-    #settings
-    CW = 1     # Clockwise Rotation
-    CCW = 0    # Counterclockwise Rotation
-    SPR = 200   # Steps per Revolution (360 / 7.5)
-
+def setup(step, dir, mos):
+  
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(DIR, GPIO.OUT)
-    GPIO.setup(STEP, GPIO.OUT)
-    GPIO.setup(DIR2, GPIO.OUT)
-    GPIO.setup(STEP2, GPIO.OUT)
-    GPIO.setup(MOS, GPIO.OUT)
-    GPIO.setup(MOS2, GPIO.OUT)
-    GPIO.output(DIR, CW)
-    GPIO.output(MOS, GPIO.LOW)
-    GPIO.output(DIR2, CW)
-    GPIO.output(MOS2, GPIO.LOW)
-
+    GPIO.setup(dir, GPIO.OUT)
+    GPIO.setup(step, GPIO.OUT)
+    GPIO.setup(mos, GPIO.OUT)
+    GPIO.output(mos, GPIO.LOW)
 
     MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
     GPIO.setup(MODE, GPIO.OUT)
