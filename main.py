dd9bb707cf934fe067d5affa3701c8febd0fdf35
jurij1 <96693230+Dogegowow@users.noreply.github.com>
2022-05-05 15:09:00 +0000

    list in funkcije z id

--- main.py
+++ main.py
@@ -12,55 +12,38 @@ def home():
   print(dir(motor))
   return render_template("index.html")
 
-@app.route("/motor1")
-def run_motor():
-  print("Motor 1 pognan")
+@app.route("/motor/<id>")
+def run_motor(id):
+  print("Motor " + id + " pognan")
+  motors = [
+    {
+      "dir": 16,
+      "step": 20,
+      "mos": 23
+    },
+    {
+      "dir": 5,
+      "step": 6,
+      "mos": 25
+    },
+    {
+      "dir": 26,
+      "step": 19,
+      "mos": 24
+    }
+    
+  ]
 
-  #motor1
-  DIR = 16   # Direction GPIO Pin
-  STEP = 20  # Step GPIO Pin
-  MOS = 23
   #settings
   CW = 1     # Clockwise Rotation
   CCW = 0    # Counterclockwise Rotation
-  SPR = 200   # Steps per Revolution (360 / 7.5)
-
-  motor.setup(STEP, DIR, MOS)
-  motor.run(STEP, DIR, CCW, MOS, 1)
-  return redirect(url_for("home"))
-
-@app.route("/motor2")
-def run_motor():
-  print("Motor 2 pognan")
   
-  #motor2
-  DIR2 = 5
-  STEP2 = 6
-  MOS2 = 25
-  #settings
-  CW = 1     # Clockwise Rotation
-  CCW = 0    # Counterclockwise Rotation
-  SPR = 200   # Steps per Revolution (360 / 7.5)
+  #for m in motors:
+  motor.setup(motors[int(id)]["step"], motors[int(id)]["dir"], motors[int(id)]["mos"])
 
-  motor.setup(STEP2, DIR2, MOS2)
-  motor.run(STEP2, DIR2, CCW, MOS2, 1)
-  return redirect(url_for("home"))
+  #for m in motors:
+  motor.run(motors[int(id)]["step"], motors[int(id)]["dir"], CCW, motors[int(id)]["mos"], 1)
 
-@app.route("/motor3")
-def run_motor():
-  print("Motor 3 pognan")
-
-  #motor3
-  DIR3 = 26 
-  STEP3 = 19
-  MOS3 = 24
-  #settings
-  CW = 1     # Clockwise Rotation
-  CCW = 0    # Counterclockwise Rotation
-  SPR = 200   # Steps per Revolution (360 / 7.5)
-
-  motor.setup(STEP3, DIR3, MOS3)
-  motor.run(STEP3, DIR3, CCW, MOS3, 1)
   return redirect(url_for("home"))
 
 app.run(host='0.0.0.0', port=8080)
