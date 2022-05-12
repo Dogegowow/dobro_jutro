be0a5a8e08c3d4f3aac5883aee73c4391820bebd
jurij1 <96693230+Dogegowow@users.noreply.github.com>
2022-05-12 14:19:36 +0000

    new index

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+  return render_template("index(test).html")
 
 @app.route("/motor/<id>")
 def run_motor(id):
